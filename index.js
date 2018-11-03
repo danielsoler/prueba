61f728ff766bc951a70f3797b553877011b6df17
Daniel Soler <cdsg.6.20.13@gmail.com>
2018-11-03 18:12:40 -0500

    sql, post and get

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
-
 const Hapi = require('hapi');
 const Inert = require('inert');
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'video club',
+    password: 'Luyeinne.10',
+    port: 5432,
+});
 const server = new Hapi.Server({
     port: 3000,
     host: "localhost",
@@ -28,8 +35,24 @@ const init = async function() {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-    
             return 'Hello, world!';
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/socios',
+        handler: async(request, h) => {
+            let result = await pool.query('SELECT * FROM socios');
+            return result.rows;
+        }
+    });
+    server.route({
+        method: 'POST',
+        path: '/socios',
+        handler: async(request, h) => {
+            let body = request.payload;
+            let result = await pool.query('INSERT  INTO socios VALUES($1, $2, $3, $4)', [body.codigo, body.nombre, body.direccion, body.telefono]);
+            return result.rows;
         }
     });
     await server.start();
